4052099d68552d1d1da6100ed1a98893b95d21b5
Ryan Durso <rdurso86@gmail.com>
2020-03-29 13:09:01 -0400

    added packages. created server, schema, and seeds files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # burger
+
+This application allows a user to add a burger to a list.
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Poblano Picasso Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('If Looks Could Kale Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Potato Burger', false);
