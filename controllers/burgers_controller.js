4f22c1db1101a07ef3a4f15f1f2af5109f22d07f
Ryan Durso <rdurso86@gmail.com>
2020-03-29 16:47:46 -0400

    fixed another typo, that wasn't the issue. Back to saying the table does not exist

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ let express = require("express");
 let router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-let burger = require("../models/burger.js");
+let burgers = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
