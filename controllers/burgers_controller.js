39ed35ebd17d68d0ca3f2e79ed5359e5561c0b69
Ryan Durso <rdurso86@gmail.com>
2020-03-29 16:45:59 -0400

    was getting error when trying to run server, fixed some typos in controller. Now it seems to let the server run, but getting an error telling me burgers is not defined

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ let burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burgers.all(function(data) {
     let hbsObject = {
       burgers: data
     };
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  cat.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+  burgers.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -28,7 +28,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update(
+  burgers.update(
     {
       devoured: req.body.devoured
     },
@@ -47,7 +47,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
     let condition = "id = " + req.params.id;
 
-    burger.delete(condition, function(result) {
+    burgers.delete(condition, function(result) {
           if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
